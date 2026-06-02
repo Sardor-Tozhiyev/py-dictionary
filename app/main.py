4c35cf359726c1c0e5d22ace435777a212249759
Sardor <sardor.tozhiev1994@gmail.com>
2026-06-02 15:10:42 +0300

    Replaced size -> length, table -> hash_table, None -> Any Added message to KeyError

--- app/main.py
+++ app/main.py
@@ -7,47 +7,47 @@ class Dictionary:
 
     def __init__(self) -> None:
         self.capacity = self.initial_capacity
-        self.size = 0
-        self.table: list = [None] * self.capacity
+        self.length = 0
+        self.hash_table: list = [None] * self.capacity
 
     def _resize(self) -> None:
-        old_table = self.table
+        old_table = self.hash_table
         self.capacity *= 2
-        self.table = [None] * self.capacity
-        self.size = 0
+        self.hash_table = [None] * self.capacity
+        self.length = 0
 
         for item in old_table:
             if item is not None:
                 self.__setitem__(item[0], item[2])
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        if self.size >= self.capacity * self.load_factor:
+        if self.length >= self.capacity * self.load_factor:
             self._resize()
 
         hash_value = hash(key)
         index = hash_value % self.capacity
 
-        while self.table[index] is not None:
-            if (self.table[index][1] == hash_value
-                    and self.table[index][0] == key):
-                self.table[index] = (key, hash_value , value)
+        while self.hash_table[index] is not None:
+            if (self.hash_table[index][1] == hash_value
+                    and self.hash_table[index][0] == key):
+                self.hash_table[index] = (key, hash_value , value)
                 return
             index = (index + 1) % self.capacity
 
-        self.table[index] = (key, hash_value , value)
-        self.size += 1
+        self.hash_table[index] = (key, hash_value , value)
+        self.length += 1
 
-    def __getitem__(self, key: Any) -> None:
+    def __getitem__(self, key: Any) -> Any:
         hash_value = hash(key)
         index = hash_value % self.capacity
 
-        while self.table[index] is not None:
-            if (self.table[index][1] == hash_value
-                    and self.table[index][0] == key):
-                return self.table[index][2]
+        while self.hash_table[index] is not None:
+            if (self.hash_table[index][1] == hash_value
+                    and self.hash_table[index][0] == key):
+                return self.hash_table[index][2]
             index = (index + 1) % self.capacity
 
-        raise KeyError(key)
+        raise KeyError(f"Key '{key}' not found in dictionary")
 
     def __len__(self) -> int:
-        return self.size
+        return self.length
